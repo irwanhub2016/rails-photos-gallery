5dfc5fd458683fbc6982ea763fe7d8beda483456
Irwan Syarifudin <irwan.syarifudin@sleekr.co>
2021-09-17 07:19:12 +0700

    add major files

---
+++ db/seeds.rb
@@ -0,0 +1,15 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+Photo.destroy_all
+
+p "Destroyed #{Photo.count} photos"
+
+Photo.create(path: '/home', caption: 'bebas')
+
+p "Created #{Photo.count} photos"
